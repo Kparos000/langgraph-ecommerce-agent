17ded5b1919f8535eae923a82729de7b92182d90
KP Akpomiemie <kparos14@outlook.com>
2025-09-19 22:10:46 +0200

    Updated state.py with AgentState TypedDict using operator.add for messages annotation

--- state.py
+++ state.py
@@ -1,8 +1,9 @@
 from typing import TypedDict, Annotated, List, Dict
-from langchain_core.messages import BaseMessage, add_messages
+from langchain_core.messages import BaseMessage
+from operator import add
 
 class AgentState(TypedDict):
-    messages: Annotated[List[BaseMessage], add_messages]
+    messages: Annotated[List[BaseMessage], add]
     remaining_steps: str
     memory: str
     schema: str  # JSON string of table schemas
